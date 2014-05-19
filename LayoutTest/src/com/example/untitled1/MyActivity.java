e42421db6188eede62b835065f5c77bee8f84783
sgorokhovich <sgorokhovich@gmail.com>
2014-05-19 13:28:40 +0300

    Working with button click listener

--- LayoutTest/src/com/example/untitled1/MyActivity.java
+++ LayoutTest/src/com/example/untitled1/MyActivity.java
@@ -2,6 +2,8 @@ package com.example.untitled1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
@@ -15,7 +17,7 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        TextView myTextView = (TextView) findViewById(R.id.myTextView);
+        final TextView myTextView = (TextView) findViewById(R.id.myTextView);
         myTextView.setText("New custom text in textView");
 
         Button myButton = (Button) findViewById(R.id.myButton);
@@ -24,5 +26,30 @@ public class MyActivity extends Activity {
 
         CheckBox myCheckBox = (CheckBox) findViewById(R.id.myCheckBox);
         myCheckBox.setChecked(true);
+
+        //ok & cancel buttons (Lesson 8)
+
+        Button okButton = (Button) findViewById(R.id.okButton);
+        Button cancelbutton = (Button) findViewById(R.id.cancelButton);
+
+        okButton.setOnClickListener(onClickListener);
+        cancelbutton.setOnClickListener(onClickListener);
     }
+
+    private OnClickListener onClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.okButton: {
+                    TextView myTextView = (TextView) findViewById(R.id.myTextView);
+                    myTextView.setText("Ok button pressed!");
+                } break;
+                case R.id.cancelButton: {
+                    TextView myTextView = (TextView) findViewById(R.id.myTextView);
+                    myTextView.setText("Cancel button pressed!");
+                } break;
+                default: break;
+            }
+        }
+    };
 }
